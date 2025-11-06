0a8057b66d3089b65e8eec8cceeaa61b9ecbdec5
AL4ONE <alfianardiansyah2804@gmail.com>
2025-11-06 23:01:31 +0700

    chore(branding): replace logo-stiqr.jpg and logo-stiqr2.jpg with logoNew.png across all components

--- src/layouts/full/shared/logo/Logo.js
+++ src/layouts/full/shared/logo/Logo.js
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router';
-import logoStiqr2 from 'src/assets/images/logos/logo-stiqr2.jpg';
+import { Link } from 'react-router-dom';
+import logoNew from 'src/assets/images/logos/logoNew.png';
 import { styled } from '@mui/material';
 
 const Logo = () => {
@@ -23,7 +23,7 @@ const Logo = () => {
       }}
     >
       <img
-        src={logoStiqr2}
+        src={logoNew}
         alt="StiqrHub Logo"
         style={{
           width: '100%',
