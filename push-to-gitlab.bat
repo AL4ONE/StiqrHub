e3829cfa0c9f29d9e0167c404a0627217b6812c1
AL4ONE <alfianardiansyah2804@gmail.com>
2025-11-25 16:47:09 +0700

    Update push script: Frontend selalu push ke branch on-development di gitlab-fe

--- push-to-gitlab.bat
+++ push-to-gitlab.bat
@@ -1,16 +1,19 @@
 @echo off
 REM Script untuk push ke GitLab FE dan BE secara terpisah
 REM FE hanya push folder frontend, BE hanya push folder backend
+REM FE selalu push ke branch on-development
+REM BE push ke branch yang ditentukan (default: deployment)
 
-set BRANCH=%1
-if "%BRANCH%"=="" set BRANCH=deployment
+set FE_BRANCH=on-development
+set BE_BRANCH=%1
+if "%BE_BRANCH%"=="" set BE_BRANCH=deployment
 
-echo Pushing frontend to GitLab FE...
-git subtree push --prefix=frontend gitlab-fe %BRANCH%
+echo Pushing frontend to GitLab FE (branch: %FE_BRANCH%)...
+git subtree push --prefix=frontend gitlab-fe %FE_BRANCH%
 
 echo.
-echo Pushing backend to GitLab BE...
-git subtree push --prefix=backend gitlab-be %BRANCH%
+echo Pushing backend to GitLab BE (branch: %BE_BRANCH%)...
+git subtree push --prefix=backend gitlab-be %BE_BRANCH%
 
 echo.
 echo Done! Frontend dan Backend sudah di-push ke GitLab masing-masing.
