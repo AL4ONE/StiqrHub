93f970bfc6eafd635ccee8241b8ffd0148165ff5
AL4ONE <alfianardiansyah2804@gmail.com>
2025-11-27 13:22:03 +0700

    Move src folder to frontend/src in monorepo structure

--- src/utils/auth.js
+++
@@ -1,7 +0,0 @@
-export const getAuthHeaders = () => {
-  const token = localStorage.getItem('token'); // atau 'access_token' sesuai penyimpanan lu
-  return {
-    Authorization: `Bearer ${token}`,
-  };
-};
- 
